608ee8d8df17eee86c972436b15f7085a13d5977
dev-vm\user01 <zhen.yuan@microsoft.com>
2023-07-05 13:35:52 +0000

    inital commit

---
+++ Poc.AspNetCore.SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace AspNetCore.SignalR {
+    
+    public class MessageHub: Hub {
+
+        public async Task SendMessage(string user, string message)
+            => await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        public Task SendMessageToGroup(string groupName, string message)
+        {
+            return Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId}: {message}");
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+        public Task SendPrivateMessage(string user, string message)
+        {
+            return Clients.User(user).SendAsync("ReceiveMessage", message);
+        }
+    }
+}
